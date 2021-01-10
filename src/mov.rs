a6e52df3156df53f5ee1d564e5b2919a7d80e945
Tomasz Cichocki <cichymail@gmail.com>
2021-01-10 20:54:33 +0100

    command type in the first byte of the message

--- src/mov.rs
+++ src/mov.rs
@@ -2,20 +2,31 @@ use rumqttc::{Client, QoS};
 use std::thread;
 use std::time::Duration;
 use byteorder::{ByteOrder, LittleEndian};
+use crate::mov::CommandType::{MOVE, GRIP};
 
-fn create_move_message(x: i16, y: i16, z: i16, s: i16) -> [u8; 8] {
-    let mut buf = [0u8; 8];
-    LittleEndian::write_i16(&mut buf, x);
-    LittleEndian::write_i16(&mut buf[2..], y);
-    LittleEndian::write_i16(&mut buf[4..], z);
-    LittleEndian::write_i16(&mut buf[6..], s);
+enum CommandType {
+    READ = 0, // TODO read current position
+    MOVE, // move to position with constant speed
+    GRIP, // change gripper position
+    MOVE_SEQ, // TODO sequence of positions (every 20 ms) [what about gripper?]
+    SERVO_SEQ // TODO sequence of direct servo positions (every 20 ms)
+}
+
+fn create_move_message(x: i16, y: i16, z: i16, s: i16) -> [u8; 9] {
+    let mut buf = [0u8; 9];
+    buf[0] = MOVE as u8;
+    LittleEndian::write_i16(&mut buf[1..], x);
+    LittleEndian::write_i16(&mut buf[3..], y);
+    LittleEndian::write_i16(&mut buf[5..], z);
+    LittleEndian::write_i16(&mut buf[7..], s);
     buf
 }
 
-fn create_grip_message(a: i16, s: i16) -> [u8; 4] {
-    let mut buf = [0u8; 4];
-    LittleEndian::write_i16(&mut buf, a);
-    LittleEndian::write_i16(&mut buf[2..], s);
+fn create_grip_message(a: i16, s: i16) -> [u8; 5] {
+    let mut buf = [0u8; 5];
+    buf[0] = GRIP as u8;
+    LittleEndian::write_i16(&mut buf[1..], a);
+    LittleEndian::write_i16(&mut buf[3..], s);
     buf
 }
 
